518ac6789e8afbf1688a3842c20dee31f22646a9
Muhammad Arslan <65185229+ArslanDevPy@users.noreply.github.com>
2022-02-26 11:31:51 +0500

    ...

--- js/script.js
+++ js/script.js
@@ -1,13 +1,78 @@
-function sendEmail() {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "arslan7980655@gmail.com",
-        Password: "AY267622",
-        To: 'arslandev.py@gmail.com',
-        From: "arslandev.py@gmail.com",
-        Subject: "Hi",
-        Body: "How r u",
-    }).then(
-        message => alert("mail sent successfully")
-    );
+const firebaseConfig = {
+    apiKey: "AIzaSyDnlNQsNuwHFgSqe6t9tCV57AxyTBDA2Uc",
+    authDomain: "github-portfolio-c0be0.firebaseapp.com",
+    projectId: "github-portfolio-c0be0",
+    storageBucket: "github-portfolio-c0be0.appspot.com",
+    messagingSenderId: "796587044513",
+    appId: "1:796587044513:web:6d7be7b73b6dc1cb2fcae0",
+    measurementId: "G-MQRX92MBKT"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const sendMessageForm = document.getElementById("send_message_form");
+
+sendMessageForm.addEventListener('change', function () {
+    if (sendMessageForm.name.value === "") {
+        sendMessageForm.name.classList.add('boder_red');
+    } else {
+        sendMessageForm.name.classList.remove('boder_red');
+    }
+
+    if (loginForm.email.value === "") {
+        sendMessageForm.email.classList.add('boder_red');
+    } else {
+        sendMessageForm.email.classList.remove('boder_red');
+    }
+
+    if (loginForm.phone.value === "") {
+        sendMessageForm.phone.classList.add('boder_red');
+    } else {
+        sendMessageForm.phone.classList.remove('boder_red');
+    }
+
+    if (loginForm.subject.value === "") {
+        sendMessageForm.subject.classList.add('boder_red');
+    } else {
+        sendMessageForm.subject.classList.remove('boder_red');
+    }
+
+    if (loginForm.message.value === "") {
+        sendMessageForm.message.classList.add('boder_red');
+    } else {
+        sendMessageForm.message.classList.remove('boder_red');
+    }
+});
+
+function send_message() {
+    const name = sendMessageForm.name;
+    const email = sendMessageForm.email;
+    const phone = sendMessageForm.phone;
+    const subject = sendMessageForm.subject;
+    const message = sendMessageForm.message;
+    if (name.value === "") {
+        name.classList.add('boder_red');
+    } if (email.value === "") {
+        email.classList.add('boder_red');
+    } if (phone.value === "") {
+        phone.classList.add('boder_red');
+    } if (subject.value === "") {
+        subject.classList.add('boder_red');
+    } if (message.value === "") {
+        message.classList.add('boder_red');
+    }
+
+    db.collection("contact").add({
+        name,
+        email,
+        phone,
+        subject,
+        message
+    })
+        .then((docRef) => {
+            alert("Successfully Submitted..!");
+        })
+        .catch((error) => {
+            alert("Error..!");
+        });
 }
